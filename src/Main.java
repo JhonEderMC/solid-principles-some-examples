475d3161e79a6678a45dfa04af208fb528b7bc81
jhonederMC <jhon.mosquera@sofka.com.co>
2024-01-21 10:25:33 -0500

    add srp test feature

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import srp.TestSrp;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // SRP
+        new TestSrp().Test();
+
+        //System.out.println("Hello world!");
     }
 }
