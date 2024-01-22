06119d2fc48383e084a439337a032c436166339e
jhonederMC <jhon.mosquera@sofka.com.co>
2024-01-21 20:11:17 -0500

    Add OCP Test

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import ocp.TestOcp;
 import srp.TestSrp;
 
 public class Main {
     public static void main(String[] args) {
 
         // SRP
-        new TestSrp().Test();
+        //new TestSrp().Test();
+        // OCP
+        TestOcp.test();
 
         //System.out.println("Hello world!");
     }
