f395cecf31f40a547104ea46057f84cc387d9eb7
jhonederMC <jhon.mosquera@sofka.com.co>
2024-01-21 19:56:53 -0500

    create DiscountFixedAmount file with logic

---
+++ src/ocp/DiscountFixedAmount.java
@@ -0,0 +1,23 @@
+package ocp;
+
+public class DiscountFixedAmount implements Discount {
+
+    private double fixAmount;
+
+    public DiscountFixedAmount(double fixAmount) {
+        this.fixAmount = fixAmount;
+    }
+
+    public double getFixAmount() {
+        return fixAmount;
+    }
+
+    public void setFixAmount(double fixAmount) {
+        this.fixAmount = fixAmount;
+    }
+
+    @Override
+    public double applyDiscount(double price) {
+        return price - this.fixAmount;
+    }
+}
