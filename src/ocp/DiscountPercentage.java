3660cc340194696f8583ec6b7d391dffec7c0cde
jhonederMC <jhon.mosquera@sofka.com.co>
2024-01-21 19:47:32 -0500

    create DiscountPercentage class

--- src/ocp/DiscountPercentage.java
+++ src/ocp/DiscountPercentage.java
@@ -1,2 +1,24 @@
-package ocp;public class DiscountPercentage {
+package ocp;
+
+public class DiscountPercentage implements Discount {
+
+    private int percentage;
+
+
+    public DiscountPercentage(int percentage) {
+        this.percentage = percentage;
+    }
+
+    public int getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(int percentage) {
+        this.percentage = percentage;
+    }
+
+    @Override
+    public double applyDiscount(double price) {
+        return (price - (price * ((double) this.percentage /100)));
+    }
 }
