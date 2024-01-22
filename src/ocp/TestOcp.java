9e2059b2b929988f0fdf2b90d8aed1b13e881026
jhonederMC <jhon.mosquera@sofka.com.co>
2024-01-21 20:10:06 -0500

    createTestOcp class to test OCP principle

--- src/ocp/TestOcp.java
+++ src/ocp/TestOcp.java
@@ -1,2 +1,16 @@
-package ocp;public class TestOcp {
+package ocp;
+
+public class TestOcp {
+
+    public static void test() {
+        DiscountPercentage discountPercentage = new DiscountPercentage(7);
+        DiscountFixedAmount discountFixedAmount = new DiscountFixedAmount(40);
+
+        System.out.println("100$ will have discount to 7%: " + TestOcp.applyDiscount(discountPercentage, 100));
+        System.out.println("100$ will have discount to 40$: " + TestOcp.applyDiscount(discountFixedAmount, 100));
+    }
+
+    public static double applyDiscount(Discount discount, double price) {
+        return discount.applyDiscount(price);
+    }
 }
