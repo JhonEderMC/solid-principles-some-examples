cf992d4cfed96cd15b65d3648871d1f17cf39c58
jhonmocc <jhon.mosquera@sofka.com.co>
2024-05-13 11:56:11 -0500

    delete import that not uses

--- src/test/java/lsp/CalculateAreaTest.java
+++ src/test/java/lsp/CalculateAreaTest.java
@@ -1,2 +1,29 @@
-package test.java.lsp;public class CalculateAreaTest {
+package test.java.lsp;
+
+import main.java.lsp.CalculateArea;
+import main.java.srp.Circle;
+import main.java.srp.Form;
+import main.java.srp.GeometryForm;
+import main.java.srp.Rectangle;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculateAreaTest {
+
+    @Test
+    public void calculateSumAreaTest() {
+        //Arrange
+        Rectangle rectangle = new Rectangle(2, 2);
+        Circle circle = new Circle(3);
+        Form [] forms = {rectangle, circle};
+
+        //Act
+        double areaCalculate = CalculateArea.calculateTotalArea(forms);
+        double areaEstimated = GeometryForm.getArea(rectangle) + GeometryForm.getArea(circle);
+
+        //Assert
+        assertEquals(areaCalculate, areaEstimated);
+    }
 }
