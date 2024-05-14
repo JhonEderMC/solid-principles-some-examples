d04bfbb3a301188f2e452593a4fd0562f23cd8cb
jhonmocc <jhon.mosquera@sofka.com.co>
2024-05-14 11:37:46 -0500

    create EmailService and SmsService.java that implementation of NotificationService

--- src/main/java/dip/EmailService.java
+++ src/main/java/dip/EmailService.java
@@ -1,2 +1,9 @@
-package main.java.dip;public class ServicioEmail {
+package main.java.dip;
+
+public class EmailService implements  NotificationService {
+
+    @Override
+    public void send(String message) {
+        System.out.println("The email was sent: "+ message);
+    }
 }
--- src/main/java/dip/SmsService.java
+++ src/main/java/dip/SmsService.java
@@ -1,2 +1,8 @@
-package main.java.dip;public class SmsService {
+package main.java.dip;
+
+public class SmsService implements  NotificationService {
+    @Override
+    public void send(String message) {
+        System.out.println("The sms was sent");
+    }
 }
