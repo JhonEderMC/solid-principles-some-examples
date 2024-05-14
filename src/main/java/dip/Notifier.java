6c51c1292e8e8a8b508f50a86608f9be2306c0f4
jhonmocc <jhon.mosquera@sofka.com.co>
2024-05-14 11:39:40 -0500

    Notifier.java is a class that management notifications

--- src/main/java/dip/Notifier.java
+++ src/main/java/dip/Notifier.java
@@ -1,2 +1,14 @@
-package main.java.dip;public class Notifier {
+package main.java.dip;
+
+public class Notifier {
+
+    private final NotificationService notificationService;
+
+    public Notifier(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    public void sendNotifie(String messsage) {
+        this.notificationService.send(messsage);
+    }
 }
